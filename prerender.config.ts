4ef5422119e545d76a1506d54be8d1affd486320
corysmc <cory.s.mcarthur@gmail.com>
2022-11-11 15:53:06 -0700

    Fixing prerendering trailing slashes

--- prerender.config.ts
+++ prerender.config.ts
@@ -1,4 +1,18 @@
 import { PrerenderConfig } from '@stencil/core';
 export const config: PrerenderConfig = {
   trailingSlash: false,
+  filePath: (url, filePath) => {
+    const pathName = url.pathname;
+    if (pathName === '/') {
+      return filePath;
+    }
+    const path = `${pathName}.html`;
+    const arr = filePath.split('/');
+    arr.pop();
+    arr.pop();
+    const newPath = arr.join('/') + path;
+    console.log({ url, path, filePath, newPath });
+
+    return newPath;
+  },
 };
